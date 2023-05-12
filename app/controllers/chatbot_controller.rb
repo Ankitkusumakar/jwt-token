7ed31d9c2f9d2709e68f826f6f74cca6b69ad289
Ankitkusumakar <124240062+Ankitkusumakar@users.noreply.github.com>
2023-05-12 19:26:43 +0530

    Update chatbot_controller.rb

--- app/controllers/chatbot_controller.rb
+++ app/controllers/chatbot_controller.rb
@@ -2,7 +2,7 @@ class ChatbotController < ApplicationController
 	skip_before_action :verify_authenticity_token
   def chat
     return render json: {error: "please pass messgae"} unless params[:message].present?
-    client = OpenAI::Client.new(access_token: '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')
+    client = OpenAI::Client.new(access_token: 'sk-SK')
     response = client.chat(
       parameters: {
           model: "gpt-3.5-turbo", # Required.
